b89a1327ff2979c4e301f1d1ec475fa82e122d5c
RoNaranjo02 <130687333+RoNaranjo02@users.noreply.github.com>
2023-06-07 23:43:16 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from claseManejador import manejadorVehiculos
+from menu import menuOpciones
+from claseTest import TestVehiculos
+import unittest
+
+if __name__ == '__main__':
+    
+    unittest.main()
